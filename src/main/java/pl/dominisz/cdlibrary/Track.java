363c4a7edab113bb5e295634d5e60912d4dc1101
Dagmara Skrzat <dagmara167@op.pl>
2018-04-05 18:13:29 +0200

    Track class was added, Genre enum was created

---
+++ src/main/java/pl/dominisz/cdlibrary/Track.java
@@ -0,0 +1,14 @@
+package pl.dominisz.cdlibrary;
+
+import java.time.LocalTime;
+
+public class Track {
+
+    private String title;
+    private int timeOfTrack;   //czas bedziemy przechowywali w sekundach
+    private String artist;
+    private String genre;
+
+
+
+}
